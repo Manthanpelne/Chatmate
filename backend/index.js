79a5f0baf5d46388c99a7bb8922380f2c812d8c3
Manthanpelne <111408525+Manthanpelne@users.noreply.github.com>
2024-08-13 14:56:58 +0530

    updated frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -21,17 +21,7 @@ app.use("/api/message", messageRoutes)
 
 
 //deploymentcode
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname1, "/dist")))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname1, "dist" , "index.html"))
-    })
-}else{
-    app.get("/",(req,res)=>{
-        res.send("API is running successfully")
-    })
-}
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 
 //for error handling
